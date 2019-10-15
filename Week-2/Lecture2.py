0ed3d9fa2bfcd80cda50aa0a5fda31207ffd67c1
Tristone100 <56586217+Tristone100@users.noreply.github.com>
2019-10-15 12:07:45 +0100

    Added from lecture 2

---
+++ Week-2/Lecture2.py
@@ -0,0 +1,8 @@
+name = str(input('what is your name'))
+print ('Hello ',name,' I challenge you to a little game')
+print ('lets play rock paper scissors')
+
+print ('when inputing you asnswer please note Rock is 1, Paper is 2 and Scissors is 3')
+import random
+cpu = randint(1,4)
+player = int(input('make your sellection now'))
